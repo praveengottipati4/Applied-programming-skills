c3cdd2520180becfe59fa7140cab168a023bddb7
praveengottipati4 <vtu26416@veltech.edu.in>
2026-04-09 09:46:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (49.18%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,15 +1,13 @@
 class Solution {
+
     public boolean rotateString(String s, String goal) {
-        // Step 1: Check if lengths are the same. 
-        // If they aren't, one can't be a rotation of the other.
+        // lengths must be equal
         if (s.length() != goal.length()) {
             return false;
         }
 
-        // Step 2: Double the string s
+        // check rotation
         String doubled = s + s;
-
-        // Step 3: Check if goal is a substring of the doubled string
         return doubled.contains(goal);
     }
 }
